f0f1238012a7faf0f374d7ea9e9052da3b5b20fd
AidanStrydom <aidanracer@gmail.com>
2025-11-28 15:10:30 +0200

    updated imports

--- DJS05-SOLUTION-main/src/main.jsx
+++ DJS05-SOLUTION-main/src/main.jsx
@@ -3,11 +3,17 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
+import { FavoritesProvider } from "./context/FavoritesContext";
+import { ThemeProvider } from "./context/ThemeContext";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <ThemeProvider>
+        <FavoritesProvider>
+          <App />
+        </FavoritesProvider>
+      </ThemeProvider>
     </BrowserRouter>
   </StrictMode>
 );
